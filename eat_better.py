da75ddf14d0de25512ba9f8ee2e060508fadf325
cmigazzi <cedric.migazzi@hotmail.fr>
2018-11-29 12:15:46 +0100

    app initialized

---
+++ eat_better.py
@@ -0,0 +1,9 @@
+print("""
+##################################################
+######             EAT BETTER             ########
+# L'application pour une alimentation plus saine #
+##################################################
+
+1 - Quel aliment souhaitez-vous remplacer ?
+2 - Retrouver mes aliments substitués.
+""")
