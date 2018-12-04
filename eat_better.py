cb90b92cb0e3d7fbc2ce0847a74aaa507db7d2e9
cmigazzi <cedric.migazzi@hotmail.fr>
2018-12-04 16:24:27 +0100

    API request script done

--- eat_better.py
+++ eat_better.py
@@ -1,3 +1,6 @@
+import requests
+
+from settings import CATEGORIES, SEARCH_API_URL, FIELD_NEEDED
 
 def main():
     print("""
@@ -10,6 +13,30 @@ def main():
     2 - Retrouver mes aliments substitués.
     """)
 
+def get_api_data():
+    """This function requests the OpenFoodFact API to get data
+    
+    Returns:
+        list -- list od dictionnary that represents a product
+    """
+
+    products = []
+
+    for category in CATEGORIES:
+        url = SEARCH_API_URL + f"?search_terms={category}&search_tag=category&page_size=250&json=1"
+        json_response = requests.get(url).json()
+        products_list = json_response["products"]
+
+        for complete_product in products_list:
+            clean_product = {k:v for k,v in complete_product.items() if k in FIELD_NEEDED}
+            products.append(clean_product)
+
+    print(products, len(products), sep="\n")
+
+    return products
+
+
+
 
 if __name__ == "__main__":
-    main()
+    get_api_data()
--- settings.py
+++ settings.py
@@ -3,9 +3,37 @@
 ############
 
 # DATABASE CONNECTION 
-# change the log data to your mysql configuration
+# change the login data to your mysql configuration
 
 DB_USER = "root"
 DB_PASSWORD = "root"
 DB_HOST = "localhost"
 DB_NAME = "eat_better"
+
+# API REQUEST CONFIG
+# Choose your categories, maximum 5
+CATEGORIES = [
+#    "Snacks sucrés",
+#    "Produits laitiers",
+    "Plats préparés",
+#    "Biscuits et gâteaux",
+#    "Produits à tartiner",
+#    "Petit-déjeuners",
+#    "Desserts",
+#    "Epicerie",
+#    "Surgelés",
+#    "Viandes"
+]
+
+SEARCH_API_URL = "https://fr.openfoodfacts.org/cgi/search.pl"
+CATEGORIES_API_URL = "https://fr.openfoodfacts.org/category/"
+
+FIELD_NEEDED = [
+    "product_name_fr",
+    "categories",
+    "generic_name",
+    "stores",
+    "brands",
+    "url",
+    "nutrition_grade_fr"
+]
