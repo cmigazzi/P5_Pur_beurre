d7c339272698007cff2dbb0c435797e578423ba8
cmigazzi <cedric.migazzi@hotmail.fr>
2018-12-10 14:40:19 +0100

    populating database script is working

--- eat_better.py
+++ eat_better.py
@@ -1,6 +1,8 @@
 import requests
 
 from settings import CATEGORIES, SEARCH_API_URL, FIELD_NEEDED
+from sql_queries import save_data_API, save_products
+
 
 def main():
     print("""
@@ -13,30 +15,87 @@ def main():
     2 - Retrouver mes aliments substitués.
     """)
 
+
 def get_api_data():
     """This function requests the OpenFoodFact API to get data
-    
-    Returns:
-        list -- list od dictionnary that represents a product
-    """
 
+    Returns:
+        list -- list of dictionnary that represents a product
+    """
     products = []
 
     for category in CATEGORIES:
-        url = SEARCH_API_URL + f"?search_terms={category}&search_tag=category&page_size=250&json=1"
+        url = SEARCH_API_URL + \
+            f"?search_terms={category}&search_tag=category&page_size=250&json=1"
         json_response = requests.get(url).json()
         products_list = json_response["products"]
 
         for complete_product in products_list:
-            clean_product = {k:v for k,v in complete_product.items() if k in FIELD_NEEDED}
+            clean_product = {
+                k: v for k, v in complete_product.items() if k in FIELD_NEEDED}
             products.append(clean_product)
-
-    print(products, len(products), sep="\n")
 
     return products
 
 
+def save_data():
+    """This function save the data from OFF API to database"""
+
+    products = get_api_data()
+    all_categories = []
+    all_stores = []
+    all_brands = []
+
+    for product in products:
+        categories = clean_tag(product["categories"])
+        stores = clean_tag(product["stores"])
+        brands = clean_tag(product["brands"])
+
+        filter_categories = categories[:3]
+        product["categories"] = [category for category in enumerate(filter_categories)]
+    
+        filter_stores = stores[:2]
+        product["stores"] = [store for store in enumerate(filter_stores)]
+
+        product["brands"] = brands[0]
+    
+        all_categories += filter_categories
+        all_stores += filter_stores
+        all_brands.append(product["brands"])
+    
+    clean_categories = clean_duplicate(all_categories)
+    clean_brands = clean_duplicate(all_brands)
+    clean_stores = clean_duplicate(all_stores)
+
+    save_data_API(clean_categories, clean_brands, clean_stores)
+    save_products(products)
+
+
+def clean_tag(elmt_with_commas):
+    """This function transforms a string of elements separated by commas into a list 
+
+    Arguments:
+        elmt_with_commas {string with commas} -- elements separated by commas
+
+    Returns:
+        [list] -- elements in a list
+    """
+    elmt_list = elmt_with_commas.split(",")
+    return elmt_list
+
+
+def clean_duplicate(list_of_tags):
+    """This function delete duplicates elements in a list
+
+    Arguments:
+        list_of_tags {list} -- list with duplicates
+
+    Returns:
+        list -- list without duplicates
+    """
+    clean_list = list(set(list_of_tags))
+    return clean_list
 
 
 if __name__ == "__main__":
-    get_api_data()
+    save_data()
--- sql_queries.py
+++ sql_queries.py
@@ -9,8 +9,8 @@ from settings import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 
 # Initialization queries
 def create_database():
-    """ 
-        This function creates the databse schema 
+    """
+        This function creates the databse schema
 
         !!! DON'T FORGET to configure                       !!!
         !!! your username, password, host and database name !!!
@@ -19,19 +19,19 @@ def create_database():
     db_connection = mysql.connector.connect(user=DB_USER,
                                             password=DB_PASSWORD,
                                             host=DB_HOST)
-    
+
     cursor = db_connection.cursor()
 
     # queries
-    create_db_query = (f"CREATE DATABASE IF NOT EXISTS {DB_NAME} DEFAULT CHARACTER SET utf8;")
+    create_db_query = (
+        f"CREATE DATABASE IF NOT EXISTS {DB_NAME} DEFAULT CHARACTER SET utf8mb4;")
 
     TABLES = {}
 
-    TABLES["Category"] = f""" 
+    TABLES["Category"] = f"""
         CREATE TABLE IF NOT EXISTS {DB_NAME}.`Category` (
             `id` SMALLINT NOT NULL AUTO_INCREMENT,
             `name` VARCHAR(100) NULL,
-            `tag` VARCHAR(100) NULL,
             PRIMARY KEY (`id`))
         ENGINE = InnoDB;
         """
@@ -40,7 +40,6 @@ def create_database():
         CREATE TABLE IF NOT EXISTS {DB_NAME}.`Store` (
             `id` SMALLINT NOT NULL AUTO_INCREMENT,
             `name` VARCHAR(45) NULL,
-            `tag` VARCHAR(45) NULL,
             PRIMARY KEY (`id`))
         ENGINE = InnoDB;
         """
@@ -68,7 +67,7 @@ def create_database():
             `brand` INT NULL,
             PRIMARY KEY (`id`),
             UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-            INDEX `fk_category_1_idx` 
+            INDEX `fk_category_1_idx`
                 (`category_0` ASC, `category_1` ASC, `category_2` ASC) VISIBLE,
             INDEX `fk_category_1_idx1` (`category_1` ASC) VISIBLE,
             INDEX `fk_category_2_idx` (`category_2` ASC) VISIBLE,
@@ -134,11 +133,113 @@ def create_database():
 
     for description in TABLES.values():
         cursor.execute(description)
-    
+
     cursor.close()
     db_connection.close()
+
+# Insert data from API
+def save_data_API(categories, brands, stores):
+    """This fnction saves the data from API to database
+
+    Arguments:
+        categories {list} -- list of all categories
+        brands {list} -- list of all brands as strings
+        stores {list} -- list of all stores as strings
+    """
+    db_connection = mysql.connector.connect(user=DB_USER,
+                                            password=DB_PASSWORD,
+                                            host=DB_HOST,
+                                            database=DB_NAME)
+
+    cursor = db_connection.cursor()
+
+    # queries
+    categories_query = ("INSERT INTO Category (name) VALUES (%s)")
+    brands_query = ("INSERT INTO Brand (name) VALUES (%s)")
+    stores_query = ("INSERT INTO Store (name) VALUES (%s)")
+
+    # Execution
+    for category in categories:
+        cursor.execute(categories_query, (category,))
+    print("Categories saved !")
+    for brand in brands:
+        cursor.execute(brands_query, (brand,))
+    print("Brands saved !")
+    for store in stores:
+        cursor.execute(stores_query, (store,))
+    print("Stores saved !")
+
+    db_connection.commit()
+    cursor.close()
+    db_connection.close()
+
+
+def save_products(products):
+    db_connection = mysql.connector.connect(user=DB_USER,
+                                            password=DB_PASSWORD,
+                                            host=DB_HOST,
+                                            database=DB_NAME,
+                                            charset="utf8mb4")
+    
+    cursor = db_connection.cursor()
+
+
+    for product in products:
+        try:
+            product["nutrition_grade_fr"]
+        except KeyError:
+            product["nutrition_grade_fr"] = "n"
+        # Get number of categories
+        category_field = ", ".join(["category_"+str(id)
+                                    for (id, name) in product["categories"]])
         
+        if len(product["categories"]) >= 1:
+            category_0 = product["categories"][0][1]
+            select_category_values = f"(SELECT id FROM Category WHERE name='{category_0}' LIMIT 1)"
+            if len(product["categories"]) >= 2:
+                category_1 = product["categories"][1][1]
+                select_category_values += f", \n(SELECT id FROM Category WHERE name='{category_1}' LIMIT 1)"
+                if len(product["categories"]) == 3:
+                    category_2 = product["categories"][2][1]
+                    select_category_values += f", \n(SELECT id FROM Category WHERE name='{category_2}' LIMIT 1)"
+                    
+
+        # # Get number of stores
+        store_field = ", ".join(["store_"+str(id)
+                                 for (id, name) in product["stores"]])
+
+        if len(product["stores"]) >= 1:
+            store_0 = product["stores"][0][1]
+            select_store_values = f"(SELECT id FROM Store WHERE name='{store_0}' LIMIT 1)"
+            if len(product["stores"]) == 2:
+                store_1 = product["stores"][1][1]
+                select_store_values += f", \n(SELECT id FROM Store WHERE name='{store_1}' LIMIT 1)"
+
+        brand = product["brands"]
+        select_brand_value = f"(SELECT id FROM Brand WHERE name='{brand}' LIMIT 1)"
+
+        field_name = f"(name, description, url, nutri_score, brand, {store_field}, {category_field})"
+
+        insertion_query = f"""INSERT INTO Product {field_name} 
+                  VALUES ("{product["product_name_fr"]}", "{product["generic_name"]}", "{product["url"]}", "{product["nutrition_grade_fr"]}",
+                  {select_brand_value},
+                  {select_store_values},
+                  {select_category_values});"""
+
+        try:
+            cursor.execute(insertion_query)
+        except (KeyError, mysql.connector.errors.ProgrammingError) as e:
+            print(e)
+            # print(insertion_query)
+        except mysql.connector.errors.DataError as e:
+            print(e)
+#           print(insertion_query)
+            break
+
+    db_connection.commit()
+    cursor.close()
+    db_connection.close()
+
 
 if __name__ == "__main__":
     create_database()
-
